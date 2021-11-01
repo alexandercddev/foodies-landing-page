a46088ec40b6f871361f76dbfad432af3a5fed9e
Alexander Chi <arturochi2@hotmail.com>
2021-10-31 21:21:50 -0600

    Added style Added javascripts/app.js

--- src/assets/javascripts/app.js
+++ src/assets/javascripts/app.js
@@ -4,3 +4,22 @@
  *  @date 25/octubre/2021 
 **/
 
+document.addEventListener('DOMContentLoaded', () => { 
+    document.getElementById('year').append(new Date().getFullYear());
+    document.querySelectorAll('svg[data-src]').forEach( svg => { 
+        fetch(svg.dataset.src)
+        .then(respuesta => respuesta.text())
+        .then(xml => svg.innerHTML = xml); 
+    });
+});
+
+document.getElementById('btn-send-mail').addEventListener('click', (e) => {
+    const mail = document.getElementById('emailSend').value;
+    const subject = 'Subscribe';
+    const body = mail;
+    window.open(`mailto:arturochi2@hotmail.com?subject=${subject}&body=${body}`); 
+});
+
+document.getElementById('btn-get-code').addEventListener('click', (e) => {
+    alert('ALEXANDERCDDEV')
+});
